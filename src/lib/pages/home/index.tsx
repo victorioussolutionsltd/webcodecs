83298ff95ce1f72d3500df91341626cc7c130d0d
Piotr Malecki <ideals-carders.0k@icloud.com>
2025-08-25 05:02:22 +0100

    feat: display video feed and reproduce illumination

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,11 +1,29 @@
-import { CTASection } from './components/cta-section';
+import { useEffect, useState } from 'react';
+
+import { Video } from '@/lib/pages/home/components/video';
+
 import { SomeText } from './components/some-text';
 
 const Home = () => {
+  const [isWhite, setIsWhite] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsWhite((prev) => !prev);
+    }, 500); // Flicker every 500ms
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="flex min-h-[60vh] flex-col items-center justify-center gap-8 text-center">
+    <div
+      style={{
+        backgroundColor: isWhite ? '#fff' : '#000',
+        transition: 'background-color 0.1s linear',
+      }}
+      className="flex min-h-[60vh] flex-col items-center justify-center gap-8 text-center"
+    >
       <SomeText />
-      <CTASection />
+      <Video />
     </div>
   );
 };
