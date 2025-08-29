49c56b98612ee20318a61a9daf76f78fa4a012f0
Piotr Malecki <ideals-carders.0k@icloud.com>
2025-08-29 07:08:08 +0100

    tests: revert the change

--- setup-tests.ts
+++ setup-tests.ts
@@ -4,10 +4,10 @@ import { vi } from 'vitest';
 (globalThis as unknown as { window: unknown }).window = globalThis;
 (
   globalThis as unknown as { window: { VideoDecoder: unknown } }
-).window.VideoDecoder = (opts: {
+).window.VideoDecoder = function (opts: {
   output: (frame: unknown) => void;
   error: (err: unknown) => void;
-}) => {
+}) {
   const outputCallback = opts.output;
   return {
     configure: vi.fn(),
